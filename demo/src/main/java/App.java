a7fc516410ec497cea8fb2302c925fecb0fd2a1a
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2022-05-14 15:35:53 +0700

    Add demo module

---
+++ demo/src/main/java/App.java
@@ -0,0 +1,40 @@
+
+import io.pyroscope.api.Labels;
+import io.pyroscope.api.Labels.LabelsSet;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class App {
+    public static final int N_THREADS = 8;
+
+    public static void main(String[] args) {
+        ExecutorService executors = Executors.newFixedThreadPool(N_THREADS);
+        for (int i = 0; i < N_THREADS; i++) {
+            executors.submit(() -> {
+                Labels.run(new LabelsSet("thread_name", Thread.currentThread().getName()), () -> {
+                        while (true) {
+                            try {
+                                fib(32L);
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                                break;
+                            }
+                        }
+                    }
+                );
+            });
+        }
+    }
+
+    private static long fib(Long n) throws InterruptedException {
+        if (n == 0L) {
+            return 0L;
+        }
+        if (n == 1L) {
+            return 1L;
+        }
+        Thread.sleep(100);
+        return fib(n - 1) + fib(n - 2);
+    }
+}
