dbfe9e3e7fdb592cefe27262e072fbcc5142d551
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2022-07-06 01:10:20 +0700

    chore: javadoc formatting (#52)

--- agent/src/main/java/io/pyroscope/javaagent/api/Exporter.java
+++ agent/src/main/java/io/pyroscope/javaagent/api/Exporter.java
@@ -4,21 +4,21 @@ import io.pyroscope.javaagent.Snapshot;
 
 public interface Exporter {
     /**
-     * PyroscopeAgent expects `export` method to be synchronous to profiling schedule, and should return as fast as
-     * possible. See QueuedExporter for an asynchronous implementation example.
+     * PyroscopeAgent expects {@link Exporter#export(Snapshot)} method to be synchronous to profiling schedule, and should return as fast as
+     * possible. <br>See QueuedExporter for an asynchronous implementation example.<br>
      * Here is an example of an alternative to {@link io.pyroscope.javaagent.impl.PyroscopeExporter}
-     * {@code
+     * <pre>
      * class KafkaExporter implements Exporter {
      *     final KafkaProducer<String, String> kafkaProducer;
      *     private MyExporter(KafkaProducer<String, String> producer) {
      *         this.kafkaProducer = producer;
      *     }
-     *     @Override
+     *     &#064;Override
      *     public void export(Snapshot snapshot) {
      *         kafkaProducer.send(new ProducerRecord<>("test.app.jfr", gson.toJson(snapshot)));
      *     }
      * }
-     * }
+     * </pre>
      *
      */
     void export(Snapshot snapshot);
