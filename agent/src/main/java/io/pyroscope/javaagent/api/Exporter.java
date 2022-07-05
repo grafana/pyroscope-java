08165236f95ed931fc6fd777704f02b40a386ac5
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2022-07-06 00:39:47 +0700

    chore(docs) Add Exporter, ProfilingScheduler examples, docs (#50)

--- agent/src/main/java/io/pyroscope/javaagent/api/Exporter.java
+++ agent/src/main/java/io/pyroscope/javaagent/api/Exporter.java
@@ -3,5 +3,23 @@ package io.pyroscope.javaagent.api;
 import io.pyroscope.javaagent.Snapshot;
 
 public interface Exporter {
+    /**
+     * PyroscopeAgent expects `export` method to be synchronous to profiling schedule, and should return as fast as
+     * possible. See QueuedExporter for an asynchronous implementation example.
+     * Here is an example of an alternative to {@link io.pyroscope.javaagent.impl.PyroscopeExporter}
+     * {@code
+     * class KafkaExporter implements Exporter {
+     *     final KafkaProducer<String, String> kafkaProducer;
+     *     private MyExporter(KafkaProducer<String, String> producer) {
+     *         this.kafkaProducer = producer;
+     *     }
+     *     @Override
+     *     public void export(Snapshot snapshot) {
+     *         kafkaProducer.send(new ProducerRecord<>("test.app.jfr", gson.toJson(snapshot)));
+     *     }
+     * }
+     * }
+     *
+     */
     void export(Snapshot snapshot);
 }
