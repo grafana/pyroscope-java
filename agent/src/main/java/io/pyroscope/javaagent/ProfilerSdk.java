29990721157a17b28bee1933b327f9187bc62afd
Aleksandar Petrov <8142643+aleks-p@users.noreply.github.com>
2024-12-20 12:11:18 -0400

    Add ProfilerApi for consumption by other libraries

---
+++ agent/src/main/java/io/pyroscope/javaagent/ProfilerSdk.java
@@ -0,0 +1,28 @@
+package io.pyroscope.javaagent;
+
+import io.pyroscope.javaagent.api.ProfilerScopedContext;
+import io.pyroscope.javaagent.api.ProfilerApi;
+import io.pyroscope.javaagent.config.Config;
+import io.pyroscope.javaagent.impl.ProfilerScopedContextWrapper;
+import io.pyroscope.labels.LabelsSet;
+import io.pyroscope.labels.ScopedContext;
+
+import java.util.Map;
+
+public class ProfilerSdk implements ProfilerApi {
+
+    @Override
+    public void startProfiling() {
+        PyroscopeAgent.start(Config.build());
+    }
+
+    @Override
+    public boolean isProfilingStarted() {
+        return PyroscopeAgent.isStarted();
+    }
+
+    @Override
+    public ProfilerScopedContext createScopedContext(Map<String, String> labels) {
+        return new ProfilerScopedContextWrapper(new ScopedContext(new LabelsSet(labels)));
+    }
+}
---
+++ agent/src/main/java/io/pyroscope/javaagent/impl/ProfilerScopedContextWrapper.java
@@ -0,0 +1,24 @@
+package io.pyroscope.javaagent.impl;
+
+import io.pyroscope.javaagent.api.ProfilerScopedContext;
+import io.pyroscope.labels.ScopedContext;
+
+import java.util.function.BiConsumer;
+
+public class ProfilerScopedContextWrapper implements ProfilerScopedContext {
+    private final ScopedContext ctx;
+
+    public ProfilerScopedContextWrapper(ScopedContext ctx) {
+        this.ctx = ctx;
+    }
+
+    @Override
+    public void forEachLabel(BiConsumer<String, String> consumer) {
+        ctx.forEachLabel(consumer);
+    }
+
+    @Override
+    public void close() {
+        ctx.close();
+    }
+}
