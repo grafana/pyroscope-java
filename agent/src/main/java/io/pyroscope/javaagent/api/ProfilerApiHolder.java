c1e20f474097499d99950ebbd57708dc891c56ed
Tolyan Korniltsev <anatoly.korniltsev@grafana.com>
2026-03-04 12:32:44 +1000

    Revert "feat: add ProfilerApiHolder for cross-classloader OTel integration (#289)" (#291)
    This reverts commit 4c53a149ff825d8ff3470ac6a61b77eb8cf108cc.

--- agent/src/main/java/io/pyroscope/javaagent/api/ProfilerApiHolder.java
+++
@@ -1,7 +0,0 @@
-package io.pyroscope.javaagent.api;
-
-import java.util.concurrent.atomic.AtomicReference;
-
-public class ProfilerApiHolder {
-    public static final AtomicReference<ProfilerApi> INSTANCE = new AtomicReference<>();
-}
